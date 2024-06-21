28563d9b42e51963d7750d49fde6e9d1b13ab0ba
Yaman Khashashneh <ymankh1997@gmail.com>
2024-06-21 22:58:31 +0300

    add save the leave application form data into local storage

--- All pages/assets/javascripts/leave_aplications.js
+++ All pages/assets/javascripts/leave_aplications.js
@@ -1,10 +1,13 @@
 const url = "assets/data/employees_data.json";
-const addEmployeeForm = document.getElementById("create-leave-form")
+const addEmployeeForm = document.getElementById("create-leave-form");
 const employeeNameSelect = document.getElementById("employee-name");
 const jopTitle = document.getElementById("jop-title");
 const department = document.getElementById("department");
 const startDate = document.getElementById("startDate");
 startDate.value = new Date().toISOString().split("T")[0];
+const leaves = (localStorage.leaves && localStorage.leaves) || [];
+
+
 
 let employeeData;
 async function addEmployeeNamesOptions() {
@@ -20,6 +23,7 @@ async function addEmployeeNamesOptions() {
   });
 }
 
+// This part for the searchable select. Do not touch it unless you really know what you are doing.
 document.addEventListener("DOMContentLoaded", async function () {
   await addEmployeeNamesOptions();
   const choices = new Choices(employeeNameSelect, {
@@ -29,6 +33,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   });
 });
 
+// Auto fill the employee data in the fields
 employeeNameSelect.addEventListener("change", () => {
   const employee = employeeData.find(
     (employee) => employee["Employee ID"] === employeeNameSelect.value
@@ -37,9 +42,22 @@ employeeNameSelect.addEventListener("change", () => {
   jopTitle.value = employee["Job Title"];
 });
 
-addEmployeeForm.addEventListener("submit", event => {
-  event.preventDefault()
-  const formData = new FormData(addEmployeeForm)
-  formData.forEach((key, val)=> console.log(val, key))
-  console.log();
-})
+function crateNewLeave(leave) {
+  leaves.push(leave);
+  localStorage.leaves = JSON.stringify(leaves);
+}
+
+addEmployeeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // Get the data from the form
+  const formData = {
+    employeeId: document.getElementById("employee-name").value,
+    leaveType: document.getElementById("leaveType").value,
+    startDate: document.getElementById("startDate").value,
+    endDate: document.getElementById("endDate").value,
+    reason: document.getElementById("reason").value,
+  };
+  crateNewLeave(form);
+  form.reset()
+
+});
